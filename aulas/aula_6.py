be05c38506d449162ae77db2e80b8592eb3587fe
Bruno Porto <112905418+Brunoportofc@users.noreply.github.com>
2023-10-15 16:55:26 -0300

    Update aula_6.py

--- aulas/aula_6.py
+++ aulas/aula_6.py
@@ -1,4 +1,9 @@
-contador = 0
-while contador < 5:
-    print(contador)
-    contador += 1
+"""  Calculadora de Média: Crie um programa que 
+calcule a média de três notas fornecidas pelo usuário. """
+
+# Resposta 6
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+media = (nota1 + nota2 + nota3) / 3
+print(f"A média das notas é {media:.2f}")
