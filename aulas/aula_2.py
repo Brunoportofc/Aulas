8a3e723a1947967ab4c6dac6f941223547969703
Bruno Porto <112905418+Brunoportofc@users.noreply.github.com>
2023-10-15 16:51:04 -0300

    Update aula_2.py

--- aulas/aula_2.py
+++ aulas/aula_2.py
@@ -1,5 +1,7 @@
-inteiro = 10
-flutuante = 3.14
-texto = "Olá, mundo!"
-booleano = True
+"""  onversor de Unidades: Crie um programa que 
+converta uma quantidade de metros para centímetros. """
 
+# exemplo
+metros = float(input("Digite a quantidade em metros: "))
+centimetros = metros * 100
+print(f"{metros} metros equivalem a {centimetros} centímetros")
