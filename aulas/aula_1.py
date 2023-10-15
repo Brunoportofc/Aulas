96072b8ce331ead204bf9335d3f208dbb549a524
Bruno Porto <112905418+Brunoportofc@users.noreply.github.com>
2023-10-15 16:49:47 -0300

    Update aula_1.py

--- aulas/aula_1.py
+++ aulas/aula_1.py
@@ -1,2 +1,20 @@
-nome = "João"
-idade = 25
+"""Calculadora Simples: Crie um programa que permita ao usuário realizar 
+operações matemáticas básicas, como adição, subtração, multiplicação e divisão."""
+
+# exemplo
+num1 = float(input("Digite o primeiro número: "))
+operador = input("Digite o operador (+, -, *, /): ")
+num2 = float(input("Digite o segundo número: "))
+
+if operador == '+':
+    resultado = num1 + num2
+elif operador == '-':
+    resultado = num1 - num2
+elif operador == '*':
+    resultado = num1 * num2
+elif operador == '/':
+    resultado = num1 / num2
+else:
+    resultado = "Operador inválido"
+
+print("Resultado:", resultado)
