378c8800a2fdc0e8aa5458e910a1d3d5b98f260b
Bruno Porto <112905418+Brunoportofc@users.noreply.github.com>
2023-10-15 17:00:04 -0300

    Update aula_10.py

--- aulas/aula_10.py
+++ aulas/aula_10.py
@@ -1,8 +1,20 @@
-import threading
+"""Jogo de Adivinhação: Crie um jogo em que o programa
+escolhe um número aleatório e o usuário tenta adivinhar qual é."""
 
-def imprimir_numeros():
-    for i in range(1, 6):
-        print(i)
+# exemplo 10
+import random
 
-thread = threading.Thread(target=imprimir_numeros)
-thread.start()
+numero_secreto = random.randint(1, 100)
+tentativas = 0
+
+while True:
+    tentativa = int(input("Adivinhe o número (entre 1 e 100): "))
+    tentativas += 1
+
+    if tentativa == numero_secreto:
+        print(f"Parabéns! Você acertou o número em {tentativas} tentativas.")
+        break
+    elif tentativa < numero_secreto:
+        print("Tente um número maior.")
+    else:
+        print("Tente um número menor.")
