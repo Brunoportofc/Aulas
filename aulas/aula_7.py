5065acd97c412a7f4e0bea9fce086749c1bafe66
Bruno Porto <112905418+Brunoportofc@users.noreply.github.com>
2023-10-15 16:56:28 -0300

    Update aula_7.py

--- aulas/aula_7.py
+++ aulas/aula_7.py
@@ -1,4 +1,14 @@
-frutas = ["maçã", "banana", "laranja"]
+"""Fatorial de um Número: Crie um programa que calcule o
+fatorial de um número inteiro não negativo fornecido pelo usuário. """
 
-for fruta in frutas:
-    print(fruta)
+# exemplo 7
+numero = int(input("Digite um número inteiro não negativo: "))
+fatorial = 1
+if numero < 0:
+    print("Fatorial não definido para números negativos.")
+elif numero == 0:
+    print("O fatorial de 0 é 1")
+else:
+    for i in range(1, numero + 1):
+        fatorial *= i
+    print(f"O fatorial de {numero} é {fatorial}")
