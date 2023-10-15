cfb95b5fe7ae86e03972c03bfa954e18b56fb410
Bruno Porto <112905418+Brunoportofc@users.noreply.github.com>
2023-10-15 16:58:57 -0300

    Update aula_9.py

--- aulas/aula_9.py
+++ aulas/aula_9.py
@@ -1,4 +1,17 @@
-def saudacao(nome):
-    print(f"Olá, {nome}!")
+"""   Contagem de Vogais e Consoantes: Crie um programa que conte o número 
+de vogais e consoantes em uma frase fornecida pelo usuário.  """
 
-saudacao("Ana")
+# Resposta 9
+frase = input("Digite uma frase: ")
+vogais = 0
+consoantes = 0
+
+for letra in frase:
+    if letra.isalpha():
+        if letra.lower() in 'aeiou':
+            vogais += 1
+        else:
+            consoantes += 1
+
+print(f"Vogais: {vogais}")
+print(f"Consoantes: {consoantes}")
